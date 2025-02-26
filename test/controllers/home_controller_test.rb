c4ae4e2cbf89ec4f7bf6a398060c8ae595d89792
Mark Cooper <mark.c.cooper@outlook.com>
2025-02-25 22:39:59 -0800

    Add sign in helper for tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should redirect to login when not authenticated" do
     get home_index_url
     assert_response :redirect
     assert_redirected_to new_session_path
+  end
+
+  test "should get the home page when authenticated" do
+    sign_in(users(:admin))
+    get home_index_url
+    assert_response :success
   end
 end
