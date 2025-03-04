20b3d726a52df9ab956e554d898e6f4928d111a6
Mark Cooper <mark.c.cooper@outlook.com>
2025-03-04 15:44:05 -0800

    Delete unused js

--- app/javascript/controllers/form_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  reset() {
-    this.element.reset();
-    this.element.querySelector("[autofocus='autofocus']")?.focus();
-  }
-}
