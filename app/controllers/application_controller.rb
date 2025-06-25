08c458cf1084536c207e202d17a682fc2b89729e
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-06-25 14:16:39 -0700

    Add scout apm (#100)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :resume_session
+  before_action :set_scout_context
+
+  def set_scout_context
+    if Current.user.is_a?(User)
+      ScoutApm::Context.add_user(id: Current.user.id)
+      ScoutApm::Context.add(cspace_url: Current.user.cspace_url)
+    end
+  end
 end
