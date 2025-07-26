7666a8059a9b2bf87cd44f2069e78777d8437f04
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-07-26 11:58:01 -0700

    Activity items counter

--- app/models/data_item.rb
+++ app/models/data_item.rb
@@ -2,7 +2,7 @@ class DataItem < ApplicationRecord
   include Feedbackable
   include TransitionsStatus
 
-  belongs_to :activity
+  belongs_to :activity, counter_cache: true
   belongs_to :current_task, class_name: "Task"
 
   enum :status, {
@@ -21,7 +21,15 @@ class DataItem < ApplicationRecord
     next unless completed?
 
     next current_task.touch if current_task.progress >= 100
-    current_task.touch if rand < 0.1 # bumps task.updated_at
+    current_task.touch if rand < checkin_frequency # bumps task.updated_at
+  end
+
+  def checkin_frequency
+    item_count = activity.data_items_count
+    return 0 if item_count.zero?
+
+    # cap 5% checkin, but lower as item count increases
+    [Math.sqrt(item_count) / item_count, 0.05].min
   end
 
   def feedback_context = current_task.class.name
