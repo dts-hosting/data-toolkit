955c1bd2e4dc7535d6b3a7bcd6aa2b06b69f6d98
Roberto Reiner Uhry <reiner@duatec.com.br>
2025-09-03 17:23:33 -0300

    Initial Version

---
+++ test/e2e/tests/test.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Data Toolkit CSpace', async ({ page }) => {
+  await test.step('Required Configuration', async () => {
+    expect(process.env.DATA_TOOLKIT_URL).toBeDefined();
+
+  }, { box: true });
+
+  await test.step('Webpage Live', async () => {
+    await page.goto(process.env.DATA_TOOLKIT_URL);
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Data Toolkit/);
+  }), { box: true };
+
+  await test.step('Login', async () => {
+    // await page.goto('https://toolkit.lyrasistechnology.org/');
+
+    await page.locator('#cspace_url').fill('https://anthro.collectionspace.org/');
+    await page.locator('#email_address').fill('admin@anthro.collectionspace.org');
+    await page.locator('#password').fill('Administrator');
+    // await page.locator('button[type="submit"]').click();
+    await page.getByRole('button').click({ force: true });
+
+    await expect(page.getByText('Welcome to your activities dashboard.')).toBeVisible();
+
+    await page.context().storageState({ path: 'storageState.json' });
+  }, { box: true });
+
+});
