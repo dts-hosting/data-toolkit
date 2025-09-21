4010be29c18dbe2f3a40247ba801c18e949a56d1
Roberto Reiner Uhry <reiner@duatec.com.br>
2025-09-21 20:51:04 -0300

    Enhancements

---
+++ test/e2e/tests/page_create_update.ts
@@ -0,0 +1,38 @@
+import { Page, Locator, test, expect } from '@playwright/test';
+
+class CreateUpdateRecordsPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async navigateTo(toolkitUrl) {
+    await this.page.goto(toolkitUrl);
+    await expect(this.page).toHaveTitle(/Data Toolkit/);
+
+    // Navigate to the Create or Update Records page
+    await this.page.getByRole('link', { name: 'Create or Update Records' }).click();
+    
+    // Verify that we are on the correct page
+    await expect(this.page.getByText('New Create or Update Records')).toBeVisible();
+  }
+
+  async createUpdateRecords(file, statusExpected, countExpected) {
+    // Locate the opetion of Collection Object 
+    const option = await this.page.locator('option:has-text("collectionobject")').textContent();
+    // Select the Option 
+    await this.page.selectOption('#activity_data_config_id', option);
+
+    // Upload the CSV file
+    await this.page.locator('#activity_files').setInputFiles(file);
+    // Submit the form
+    await this.page.getByRole('button', { name: /submit/i }).click();
+
+    // Verify the success message
+    await expect(this.page.getByText('Workflow Tasks')).toBeVisible();
+    // Verify that two actions were executed successfully
+    await expect(this.page.getByText(statusExpected)).toHaveCount(countExpected);
+
+  }
+}
+
+module.exports = CreateUpdateRecordsPage;
