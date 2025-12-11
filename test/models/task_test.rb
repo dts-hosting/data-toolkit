54caa424e280dfce9025a75eecc1f893671f5a8c
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-12-10 17:57:03 -0800

    Remove unnecessary require

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,4 +1,3 @@
-require "test_helper"
 require "minitest/mock"
 
 class TaskTest < ActiveSupport::TestCase
--- test/services/csvlint_validator_test.rb
+++ test/services/csvlint_validator_test.rb
@@ -1,5 +1,3 @@
-require "test_helper"
-
 class CsvlintValidatorTest < ActiveSupport::TestCase
   def subject(file)
     CsvlintValidator.new(
