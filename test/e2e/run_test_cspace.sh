63d19f2ee01d3b1e5e87efdc3a9ce65974e0eb6a
Roberto Reiner Uhry <reiner@duatec.com.br>
2025-12-11 13:34:04 -0300

    Address CSpace concurrency with same Page as DataToolkit and improvements

---
+++ test/e2e/run_test_cspace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+DATA_TOOLKIT_URL=https://toolkit.lyrasistechnology.org/ CSPACE_URL=https://anthro.collectionspace.org/  CSPACE_ADMIN=admin@anthro.collectionspace.org CSPACE_PASSWORD=Administrator npx playwright test tests/cspace-tests.spec.ts --trace on
