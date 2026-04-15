168c76dbe4bf778970055815add82e4e7ebb1a17
Roberto Reiner Uhry <reiner@duatec.com.br>
2026-04-15 19:23:20 -0300

    Refactor and enhance PublishQueueSizeJob

--- app/jobs/publish_queue_size_job.rb
+++ app/jobs/publish_queue_size_job.rb
@@ -1,28 +1,40 @@
 require "aws-sdk-cloudwatch"
-require "solid_queue/cli"
 
 class PublishQueueSizeJob < ApplicationJob
   queue_as :default
-  limits_concurrency to: 1, key: :expired_activity_delete_job, duration: 30.minutes
+  limits_concurrency to: 1, key: :publish_queue_size_job, duration: 30.minutes
 
   CW_NAMESPACE = ENV.fetch("CW_NAMESPACE", "DataToolkit/Queue")
   CW_ENVIRONMENT = ENV.fetch("CW_ENVIRONMENT", "UAT")
 
   def perform
     Rails.logger.info "Starting PublishQueueSizeJob"
-    
-    def cloudwatch_client
-      @cloudwatch_client ||= Aws::CloudWatch::Client.new(region: ENV.fetch("AWS_REGION"))
-    end
 
-    dimensions = [{name: "Environment", value: CW_ENVIRONMENT},
-      {name: "QueueName", value: "default"}]
+    dimensions = [
+      {name: "Environment", value: CW_ENVIRONMENT},
+      {name: "QueueName", value: "default"}
+    ]
 
-    metric_data = {metric_name: "Jobs", dimensions: dimensions, value: SolidQueue::ReadyExecution.count, unit: "Count"}
+    metric_data = [{
+      metric_name: "Jobs",
+      dimensions: dimensions,
+      value: SolidQueue::ReadyExecution.count.to_f,
+      unit: "Count"
+    }]
 
     cloudwatch_client.put_metric_data(namespace: CW_NAMESPACE, metric_data: metric_data)
 
     Rails.logger.info "Completed PublishQueueSizeJob"
+  rescue => e
+    Rails.logger.error "PublishQueueSizeJob unexpected error: #{e.class} #{e.message}"
+    raise
   end
 
+  private
+
+  def cloudwatch_client
+    @cloudwatch_client ||= Aws::CloudWatch::Client.new(
+      region: ENV["AWS_REGION"]
+    )
+  end
 end
